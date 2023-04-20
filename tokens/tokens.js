f58a8be468641a4124f3681577d9dbbf66d9261d
Akegvd <akegvdake@gmail.com>
2023-04-21 00:36:10 +0700

    setup design token

---
+++ tokens/tokens.js
@@ -0,0 +1,249 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 20 Apr 2023 13:39:09 GMT
+ */
+
+module.exports = {
+  "color": {
+    "dark": {
+      "secondary": {
+        "description": "",
+        "type": "color",
+        "value": "#6032e5",
+        "blendMode": "normal",
+        "extensions": {
+          "org.lukasoppermann.figmaDesignTokens": {
+            "styleId": "S:d421c4db836275faa0db3fed7a05980db24d1eb7,",
+            "exportKey": "color"
+          }
+        },
+        "filePath": "tokens/color/base.json",
+        "isSource": true,
+        "original": {
+          "description": "",
+          "type": "color",
+          "value": "#6032e5ff",
+          "blendMode": "normal",
+          "extensions": {
+            "org.lukasoppermann.figmaDesignTokens": {
+              "styleId": "S:d421c4db836275faa0db3fed7a05980db24d1eb7,",
+              "exportKey": "color"
+            }
+          }
+        },
+        "name": "secondary",
+        "attributes": {
+          "category": "color",
+          "type": "dark",
+          "item": "secondary"
+        },
+        "path": [
+          "color",
+          "dark",
+          "secondary"
+        ]
+      },
+      "primary": {
+        "description": "",
+        "type": "color",
+        "value": "#ff0000",
+        "blendMode": "normal",
+        "extensions": {
+          "org.lukasoppermann.figmaDesignTokens": {
+            "styleId": "S:2692b8642077ba6893e533e1fb4e2cf1595bc838,",
+            "exportKey": "color"
+          }
+        },
+        "filePath": "tokens/color/base.json",
+        "isSource": true,
+        "original": {
+          "description": "",
+          "type": "color",
+          "value": "#ff0000ff",
+          "blendMode": "normal",
+          "extensions": {
+            "org.lukasoppermann.figmaDesignTokens": {
+              "styleId": "S:2692b8642077ba6893e533e1fb4e2cf1595bc838,",
+              "exportKey": "color"
+            }
+          }
+        },
+        "name": "primary",
+        "attributes": {
+          "category": "color",
+          "type": "dark",
+          "item": "primary"
+        },
+        "path": [
+          "color",
+          "dark",
+          "primary"
+        ]
+      }
+    },
+    "light": {
+      "primary": {
+        "description": "",
+        "type": "color",
+        "value": "#d9d9d9",
+        "blendMode": "normal",
+        "extensions": {
+          "org.lukasoppermann.figmaDesignTokens": {
+            "styleId": "S:0e829643ed01eea8c83e0311e58c91ca08d1036c,",
+            "exportKey": "color"
+          }
+        },
+        "filePath": "tokens/color/base.json",
+        "isSource": true,
+        "original": {
+          "description": "",
+          "type": "color",
+          "value": "#d9d9d9ff",
+          "blendMode": "normal",
+          "extensions": {
+            "org.lukasoppermann.figmaDesignTokens": {
+              "styleId": "S:0e829643ed01eea8c83e0311e58c91ca08d1036c,",
+              "exportKey": "color"
+            }
+          }
+        },
+        "name": "primary",
+        "attributes": {
+          "category": "color",
+          "type": "light",
+          "item": "primary"
+        },
+        "path": [
+          "color",
+          "light",
+          "primary"
+        ]
+      },
+      "secondary": {
+        "description": "",
+        "type": "color",
+        "value": "#abfffa",
+        "blendMode": "normal",
+        "extensions": {
+          "org.lukasoppermann.figmaDesignTokens": {
+            "styleId": "S:edef774e49f02789c272367c585d98d555ec9d87,",
+            "exportKey": "color"
+          }
+        },
+        "filePath": "tokens/color/base.json",
+        "isSource": true,
+        "original": {
+          "description": "",
+          "type": "color",
+          "value": "#abfffaff",
+          "blendMode": "normal",
+          "extensions": {
+            "org.lukasoppermann.figmaDesignTokens": {
+              "styleId": "S:edef774e49f02789c272367c585d98d555ec9d87,",
+              "exportKey": "color"
+            }
+          }
+        },
+        "name": "secondary",
+        "attributes": {
+          "category": "color",
+          "type": "light",
+          "item": "secondary"
+        },
+        "path": [
+          "color",
+          "light",
+          "secondary"
+        ]
+      }
+    }
+  },
+  "size": {
+    "font": {
+      "small": {
+        "value": "0.75rem",
+        "comment": "the small size of the font",
+        "filePath": "tokens/size/font.json",
+        "isSource": true,
+        "original": {
+          "value": "0.75",
+          "comment": "the small size of the font"
+        },
+        "name": "small",
+        "attributes": {
+          "category": "size",
+          "type": "font",
+          "item": "small"
+        },
+        "path": [
+          "size",
+          "font",
+          "small"
+        ]
+      },
+      "medium": {
+        "value": "1rem",
+        "comment": "the medium size of the font",
+        "filePath": "tokens/size/font.json",
+        "isSource": true,
+        "original": {
+          "value": "1",
+          "comment": "the medium size of the font"
+        },
+        "name": "medium",
+        "attributes": {
+          "category": "size",
+          "type": "font",
+          "item": "medium"
+        },
+        "path": [
+          "size",
+          "font",
+          "medium"
+        ]
+      },
+      "large": {
+        "value": "2rem",
+        "comment": "the large size of the font",
+        "filePath": "tokens/size/font.json",
+        "isSource": true,
+        "original": {
+          "value": "2",
+          "comment": "the large size of the font"
+        },
+        "name": "large",
+        "attributes": {
+          "category": "size",
+          "type": "font",
+          "item": "large"
+        },
+        "path": [
+          "size",
+          "font",
+          "large"
+        ]
+      },
+      "base": {
+        "value": "1rem",
+        "comment": "the base size of the font",
+        "filePath": "tokens/size/font.json",
+        "isSource": true,
+        "original": {
+          "value": "{size.font.medium.value}",
+          "comment": "the base size of the font"
+        },
+        "name": "base",
+        "attributes": {
+          "category": "size",
+          "type": "font",
+          "item": "base"
+        },
+        "path": [
+          "size",
+          "font",
+          "base"
+        ]
+      }
+    }
+  }
+};
