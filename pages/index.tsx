dd67697bb5ff422142236ff3a86f5dce8bb33cec
Akegvd <akegvdake@gmail.com>
2023-04-28 11:44:34 +0700

    order UI dark theme and light

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,23 +7,23 @@ export default function Home() {
     <main className="bg-black">
       <h1>These using Design Tokens with Tailwinds</h1>
       <div className="bg-white p-4 rounded">
-        <h2>Light</h2>
-        <div className="flex gap-3">
-          <div className="text-[#000000] w-[80px] h-[80px] flex items-center justify-center bg-light-primary">
-            Primary.
-          </div>
-          <div className="text-[#000000] w-[80px] h-[80px] flex items-center justify-center bg-light-secondary">
-            Secondary
-          </div>
-        </div>
-      </div>
-      <div className="bg-white p-4 rounded">
         <h2>Dark</h2>
         <div className="flex gap-3">
           <div className="text-[#000000] w-[80px] h-[80px] flex items-center justify-center bg-dark-primary">
             Primary.
           </div>
           <div className="text-[#000000] w-[80px] h-[80px] flex items-center justify-center bg-dark-secondary">
+            Secondary
+          </div>
+        </div>
+      </div>
+      <div className="bg-white p-4 rounded">
+        <h2>Light</h2>
+        <div className="flex gap-3">
+          <div className="text-[#000000] w-[80px] h-[80px] flex items-center justify-center bg-light-primary">
+            Primary.
+          </div>
+          <div className="text-[#000000] w-[80px] h-[80px] flex items-center justify-center bg-light-secondary">
             Secondary
           </div>
         </div>
